e61ef2080f8575adfa894fe59ca365a696fc3d9b
Eric Wing <ewing -dot- public |-at-| gmail.com>
2011-08-10 13:09:36 -0700

    Fixed bad script check in

--- BasicTest/main.lua
+++ BasicTest/main.lua
@@ -1,11 +1,20 @@
+-- This is only for our slightly modified scripts.
+-- Use require('Test.More') if you use the official lua-TestMore files
 require('More')
-plan(2) -- Specify the number of tests you plan to run. 
-local nothing = nil
+-- Specify the number of tests you plan to run. 
+plan(2) 
+local someValue = 1
 local test_count = 0
-is(someValue, 1, "someValue should be equal to 1") -- This function verifies parameter 1 is equal to parameter 2
+
+-- This function verifies parameter 1 is equal to parameter 2
+is(someValue, 1, "someValue should be equal to 1")
 test_count = test_count + 1
-isnt(someValue, nil) -- This function verifies parameter 1 is not equal to parameter 2
+
+-- This function verifies parameter 1 is not equal to parameter 2
+isnt(someValue, nil)
 test_count = test_count + 1
-done_testing(test_count) -- declare you are done testing and the number of tests you have run
+
+-- declare you are done testing and the number of tests you have run
+done_testing(test_count) 
 --os.exit() -- convenient to have to make sure the process completely ends.
 
